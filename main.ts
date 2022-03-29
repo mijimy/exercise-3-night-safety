6a09005bd5594f96bcb74e69bb7094c8a32df35e
Mohd Tarmizi <mijimy.ibrahim@gmail.com>
2022-03-29 17:28:33 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 let kecerahan_cahaya = 0
+basic.showIcon(IconNames.Heart)
+// output ke lampu luar 
+pins.digitalWritePin(DigitalPin.P1, 0)
 basic.forever(function () {
     // baca kecerahan cahaya
     kecerahan_cahaya = input.lightLevel()
     // sekiranya kecerahan cahaya kurang 500
-    // P0 guna sensor pergerakan
-    if (kecerahan_cahaya < 50 && input.pinIsPressed(TouchPin.P0)) {
+    if (kecerahan_cahaya < 50) {
         // output ke lampu luar 
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.showString("hati2 berjalan masa gelap")
         basic.pause(2000)
+    } else {
+        // output ke lampu luar 
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showIcon(IconNames.SmallHeart)
     }
-    // output ke lampu luar 
-    pins.digitalWritePin(DigitalPin.P1, 0)
 })
